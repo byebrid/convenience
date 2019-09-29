42e850d964ea0ad79b0c2fd7825c6d7fb47b9904
Lex Gallon <lexlmgallon@gmail.com>
2019-09-29 18:57:17 +1000

    Redid docstrings for 2 functions
    Redid docstrings for get_from_dict() and set_in_dict() using numpy's
    guideline for docstrings obtained from
    https://numpydoc.readthedocs.io/en/latest/format.html#numpydoc-docstring-guide

--- convenience.py
+++ convenience.py
@@ -9,7 +9,23 @@ IMMUTABLE_TYPES = (bool, int, float, tuple, str, frozenset, type(None))
 
 
 def get_from_dict(keys, dict):
-    """Gets value from dict. I.e. dict[key0][key1]..."""
+    """Retrieves value from `dict` using `keys`.
+
+    It's a neat way of obtaining a value from a dictionary without a mess of
+    nested keys, especially when using variable keys.
+
+    Parameters
+    ----------
+    keys : list of immutables
+        List of keys in descending hierarchical order to search for in `dict`.
+    dict : dict
+        A dictionary
+
+    Returns
+    -------
+    Any type
+
+    """
     val = dict.copy()
     for key in keys:
         val = val[key]
@@ -18,11 +34,30 @@ def get_from_dict(keys, dict):
 
 
 def set_in_dict(keys, value=None, dict=None):
-    """
-    Sets value in dict. I.e. dict[key0][key1][key2]... = value
-    If dict is None, then create a new dict. Default value is None.
-    Will overwrite 'unassignable' types in dict if in path; types that won't
-    allow you to assign a value.
+    """Sets `value` in `dict` using `keys`.
+
+    It's a neat way of setting a value in a dictionary without a mess of
+    nested keys, especially when using variable keys.
+
+    Will create a new dict if `dict` is None. If a key somewhere along the path
+    is mutable, it will be overwritten as an empty dict. If a key is
+    non-existent, it will be set to an empty dict.
+
+    Parameters
+    ----------
+    keys : list of immutables
+        List of keys in descending hierarchical order to search for in `dict`.
+    value : Any type, optional
+        The value to be set in `dict` using `keys`. Defaults to None.
+    dict : dict, optional
+        The dictionary to be changed. Defaults to None. If `dict` is None, then
+        creates dict.
+
+    Returns
+    -------
+    dict
+        Altered copy of `dict`. (is copy the right word?)
+
     """
     if dict is None:
         d = {}
